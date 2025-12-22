7ebd6b88d17db0dc9b36dc3a6aed85d9bc18a09e
deepikavudari <deepikavudari22@gmail.com>
2025-12-22 10:58:06 +0530

    Use products.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,32 +1,3 @@
-const products = [
-  {
-    image: "images/products/intermediate-composite-basketball.jpg",
-    name: "            Intermediate Size Basketball",
-    rating: {
-      stars: 4,
-      reviews: 87,
-    },
-    priceCents: 2095,
-  },
-  {
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      reviews: 127,
-    },
-    priceCents: 1090,
-  },
-  {
-    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-    rating: {
-      stars: 4.5,
-      reviews: 56,
-    },
-    priceCents: 799,
-  },
-];
 const productGrid = document.querySelector(".products-grid");
 products.forEach((product) => {
   const html = `<div class="product-container">
@@ -43,7 +14,7 @@ products.forEach((product) => {
             <img class="product-rating-stars"
               src="images/ratings/rating-${product.rating.stars * 10}.png">
             <div class="product-rating-count link-primary">
-              ${product.rating.reviews}
+              ${product.rating.count}
             </div>
           </div>
 
